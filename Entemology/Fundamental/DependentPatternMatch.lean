67d1fdf44de888351bfbd4a1764f81a5b747ba9a
Andres Goens <andres.goens@ed.ac.uk>
2023-08-25 18:51:20 +0100

    make example more minimal

--- Entemology/Fundamental/DependentPatternMatch.lean
+++ Entemology/Fundamental/DependentPatternMatch.lean
@@ -1,13 +1,9 @@
-inductive FloatEnvelope
-  | mk : Float -> FloatEnvelope
-
-def isZero : FloatEnvelope -> Bool
-  | .mk 0 => true
+def Float.isZero : Float -> Bool
+  | 0 => true
   | _ => false
-
 /-
 dependent elimination failed, type mismatch when solving alternative with type
-  motive (FloatEnvelope.mk (Float.ofScientific 0 false 0))
+  motive (Float.ofScientific 0 false 0)
 but expected
-  motive (FloatEnvelope.mk a)
+  motive x
 -/
